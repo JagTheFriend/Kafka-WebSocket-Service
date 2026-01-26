bcc4b54509456a22c9d5291b44431120f1263224
JagTheFriend <JagTheFriend12@gmail.com>
2026-01-26 20:11:59 +0530

    Renamed file

--- common/types/Message.go
+++ common/types/Message.go
@@ -2,6 +2,7 @@
 package types
 
 type Message struct {
+	Content    string `json:"content"`
 	ChatID     string `json:"chatId"`
 	SenderID   string `json:"senderId"`
 	ReceiverID string `json:"receiverId"`
