8751e8e2e8de6feab0b4f950be1f8736137e4379
JagTheFriend <JagTheFriend12@gmail.com>
2026-01-26 17:02:38 +0530

    Working on integrating go websockets

--- server/go.mod
+++ server/go.mod
@@ -2,6 +2,14 @@ module server
 
 go 1.25.5
 
-require golang.org/x/time v0.14.0 // indirect
+require (
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	golang.org/x/time v0.14.0 // indirect
+)
 
-require github.com/labstack/echo/v5 v5.0.0
+require (
+	github.com/gorilla/websocket v1.5.3
+	github.com/labstack/echo/v5 v5.0.0
+	github.com/segmentio/kafka-go v0.4.50
+)
--- server/internal/routes/WebSocket.go
+++ server/internal/routes/WebSocket.go
@@ -5,9 +5,12 @@ import (
 	util "kafka-client/internals"
 	"net/http"
 
+	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v5"
 	"github.com/segmentio/kafka-go"
 )
+
+var upgrader = websocket.Upgrader{}
 
 type WebSocketRoute struct {
 	e        *echo.Group
@@ -19,14 +22,49 @@ func NewWebSocketRoute(e *echo.Group) *WebSocketRoute {
 
 	return &WebSocketRoute{
 		e:        grouped,
-		consumer: util.NewKafkaReader("client-response", "client-response-group"),
+		consumer: util.NewKafkaReader("message", "client-response-group"),
 	}
 }
 
 func (h *WebSocketRoute) RegisterRoutes() {
 	h.e.GET("/health", h.healthCheck)
+	h.e.GET("/message", h.broadCastMessage)
 }
 
 func (h *WebSocketRoute) healthCheck(c *echo.Context) error {
 	return c.String(http.StatusOK, "WebSocket Route Operational")
+}
+
+func (h *WebSocketRoute) broadCastMessage(c *echo.Context) error {
+	defer h.consumer.Close()
+
+	receiverId := c.Request().Header.Get("ReceiverId")
+	if receiverId == "" {
+		return echo.NewHTTPError(http.StatusBadRequest, "Missing data")
+	}
+
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		return err
+	}
+	defer ws.Close()
+
+	for {
+		// Block until client disconnects
+		select {
+		case <-c.Request().Context().Done():
+			return c.String(http.StatusGatewayTimeout, "Gateway timeout")
+		default:
+			msg, err := h.consumer.FetchMessage(c.Request().Context())
+			if err != nil {
+				continue
+			}
+			err = ws.WriteMessage(websocket.TextMessage, msg.Value)
+			if err != nil {
+				c.Logger().Error("failed to write WS message", "error", err)
+			} else {
+				h.consumer.CommitMessages(c.Request().Context(), msg)
+			}
+		}
+	}
 }
